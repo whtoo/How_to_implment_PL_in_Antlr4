62809d2a69891287206c91adeb20217a0c961c58
whtoo <whtoo@qq.com>
2020-10-18 20:37:36 +0800

    1. 初始化

---
+++ epsode1/HelloListener.java
@@ -0,0 +1,8 @@
+// Generated from Hello.g4 by ANTLR 4.0
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface HelloListener extends ParseTreeListener {
+	void enterR(HelloParser.RContext ctx);
+	void exitR(HelloParser.RContext ctx);
+}
---
+++ epsode1/HelloParser.java
@@ -0,0 +1,88 @@
+// Generated from Hello.g4 by ANTLR 4.0
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class HelloParser extends Parser {
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, ID=2, WS=3;
+	public static final String[] tokenNames = {
+		"<INVALID>", "'hello'", "ID", "WS"
+	};
+	public static final int
+		RULE_r = 0;
+	public static final String[] ruleNames = {
+		"r"
+	};
+
+	@Override
+	public String getGrammarFileName() { return "Hello.g4"; }
+
+	@Override
+	public String[] getTokenNames() { return tokenNames; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public HelloParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+	public static class RContext extends ParserRuleContext {
+		public TerminalNode ID() { return getToken(HelloParser.ID, 0); }
+		public RContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_r; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof HelloListener ) ((HelloListener)listener).enterR(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof HelloListener ) ((HelloListener)listener).exitR(this);
+		}
+	}
+
+	public final RContext r() throws RecognitionException {
+		RContext _localctx = new RContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_r);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(2); match(1);
+			setState(3); match(ID);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\2\3\5\b\4\2\t\2\3\2\3\2\3\2\3\2\2\3\2\2\2\6\2\4\3\2\2\2\4\5\7\3\2\2\5"+
+		"\6\7\4\2\2\6\3\3\2\2\2\2";
+	public static final ATN _ATN =
+		ATNSimulator.deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+	}
+}

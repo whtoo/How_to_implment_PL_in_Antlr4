99c6dbf1f79e77fcb2c31357c396c41ca4a8dff6
whtoo <whtoo@qq.com>
2020-10-18 22:10:22 +0800

    1. 重构目录结构；2。增加ant脚本；3。增加转译demo

--- epsode1/Hello.g4
+++
@@ -1,4 +0,0 @@
-grammar Hello;
-r : 'hello' ID;
-ID : [a-z]+;
-WS : [ \t\n\r]+ -> skip;
--- epsode1/HelloListener.java
+++
@@ -1,8 +0,0 @@
-// Generated from Hello.g4 by ANTLR 4.0
-import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.runtime.Token;
-
-public interface HelloListener extends ParseTreeListener {
-	void enterR(HelloParser.RContext ctx);
-	void exitR(HelloParser.RContext ctx);
-}

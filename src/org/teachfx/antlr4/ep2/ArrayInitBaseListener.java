4a3aa81d2f36f7fd3382b743fecfa609e3d37c67
whtoo <whtoo@qq.com>
2020-10-18 22:11:20 +0800

    1. 重构目录结构；2。增加ant脚本；3。增加转译demo

---
+++ src/org/teachfx/antlr4/ep2/ArrayInitBaseListener.java
@@ -0,0 +1,20 @@
+// Generated from ArrayInit.g4 by ANTLR 4.0
+package org.teachfx.antlr4.ep2;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.ErrorNode;
+
+public class ArrayInitBaseListener implements ArrayInitListener {
+	@Override public void enterArray(ArrayInitParser.ArrayContext ctx) { }
+	@Override public void exitArray(ArrayInitParser.ArrayContext ctx) { }
+
+	@Override public void enterValue(ArrayInitParser.ValueContext ctx) { }
+	@Override public void exitValue(ArrayInitParser.ValueContext ctx) { }
+
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	@Override public void visitTerminal(TerminalNode node) { }
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/org/teachfx/antlr4/ep2/ArrayInitListener.java
@@ -0,0 +1,12 @@
+// Generated from ArrayInit.g4 by ANTLR 4.0
+package org.teachfx.antlr4.ep2;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface ArrayInitListener extends ParseTreeListener {
+	void enterArray(ArrayInitParser.ArrayContext ctx);
+	void exitArray(ArrayInitParser.ArrayContext ctx);
+
+	void enterValue(ArrayInitParser.ValueContext ctx);
+	void exitValue(ArrayInitParser.ValueContext ctx);
+}

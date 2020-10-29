cdca976c65727325777231c1c842109915f91596
whtoo <whtoo@qq.com>
2020-10-29 16:23:47 +0800

    1.增加var节点和assign节点

---
+++ src/org/teachfx/antlr4/ep12/AdditionNode.java
@@ -0,0 +1,12 @@
+package org.teachfx.antlr4.ep12;
+public class AdditionNode extends InfixExpressionNode {
+    public AdditionNode() {
+        super();
+    }
+
+    public AdditionNode(ExpressionNode left, ExpressionNode right) {
+        super(left, right);
+
+    }
+
+}
---
+++ src/org/teachfx/antlr4/ep12/VarNode.java
@@ -0,0 +1,9 @@
+package org.teachfx.antlr4.ep12;
+
+public class VarNode extends ExpressionNode {
+    protected String name;
+    public VarNode(String name) {
+        super();
+        this.name = name;
+    }    
+}

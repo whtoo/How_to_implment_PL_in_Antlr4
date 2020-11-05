372a20ff5b471b9fb83b046eb8a0936de60e62fc
whtoo <whtoo@qq.com>
2020-11-05 15:19:57 +0800

    1. 去除无关目录。

--- src/org/teachfx/antlr4/ep14/cymbol/lang/ast/Ast.java
+++
@@ -1,5 +0,0 @@
-package org.teachfx.antlr4.ep14.cymbol.lang.ast;
-
-public abstract class Ast {
-    
-}
--- src/org/teachfx/antlr4/ep14/cymbol/lang/symtab/Type.java
+++
@@ -1,6 +0,0 @@
-package org.teachfx.antlr4.ep14.cymbol.lang.symtab;
-
-public interface Type {
-    public String getName();
-    public boolean isPrimitive();
-}

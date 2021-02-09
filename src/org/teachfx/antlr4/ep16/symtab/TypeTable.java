2c560b262eb5fb92f71220a17c8f7384e6dc71e0
whtoo <whtoo@qq.com>
2021-02-09 10:44:54 +0800

    1. 增加类型表管理内置类型定义。

---
+++ src/org/teachfx/antlr4/ep16/symtab/TypeTable.java
@@ -0,0 +1,14 @@
+package org.teachfx.antlr4.ep16.symtab;
+
+public class TypeTable {
+    public static BuiltInTypeSymbol INT = new BuiltInTypeSymbol("int");
+    public static BuiltInTypeSymbol FLOAT = new BuiltInTypeSymbol("float");
+    public static BuiltInTypeSymbol DOUBLE = new BuiltInTypeSymbol("double");
+    public static BuiltInTypeSymbol CHAR = new BuiltInTypeSymbol("char");
+    public static BuiltInTypeSymbol VOID = new BuiltInTypeSymbol("void");
+    public static BuiltInTypeSymbol NULL = new BuiltInTypeSymbol("null");
+    public static BuiltInTypeSymbol BOOLEAN = new BuiltInTypeSymbol("bool");
+
+
+
+}

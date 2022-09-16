f298bd7e69cdeef899df431b8e993ae24a56e429
willson.wang <drdangran@gmail.com>
2022-09-16 19:50:16 +0800

    1. remove unused dir

--- src/org/teachfx/antlr4/ep19/stackvm/StructSpace.java
+++
@@ -1,9 +0,0 @@
-package org.teachfx.antlr4.ep18.stackvm;
-
-import java.util.Arrays;
-
-public class StructSpace {
-    Object[] fields;
-    public StructSpace(int nfields) { this.fields = new Object[nfields]; }
-    public String toString() { return Arrays.toString(fields); }
-}

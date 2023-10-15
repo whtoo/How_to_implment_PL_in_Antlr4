302bf2a30462969c17d9c8f13f7bcd5446a0c203
willson walter <whtoo@qq.com>
2023-10-15 21:48:54 +0800

    1. [FEATURE]::init a BasicBlock.

---
+++ ep20/src/main/java/org/teachfx/antlr4/ep20/pass/cfg/BasicBlock.java
@@ -0,0 +1,28 @@
+package org.teachfx.antlr4.ep20.pass.cfg;
+
+import org.teachfx.antlr4.ep20.ir.IRNode;
+import org.teachfx.antlr4.ep20.ir.stmt.Label;
+
+import java.util.List;
+
+public class BasicBlock {
+
+    List<IRNode> stmts = List.of();
+
+    public boolean isEmpty() {
+        return stmts.isEmpty();
+    }
+
+    public void addStmt(IRNode stm) {
+        stmts.add(stm);
+    }
+
+    public List<IRNode> getStmts() {
+        return stmts;
+    }
+
+    // Get size of `stmts`
+    public int size() {
+        return stmts.size();
+    }
+}

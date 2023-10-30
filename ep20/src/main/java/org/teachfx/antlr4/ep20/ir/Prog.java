a9657caa25cdb9c6d792e511b9e754987c3519dc
willson walter <whtoo@qq.com>
2023-10-30 16:08:11 +0800

    1. Add linear IR instructions in Prog for CFG analysis

--- ep20/src/main/java/org/teachfx/antlr4/ep20/ir/Prog.java
+++ ep20/src/main/java/org/teachfx/antlr4/ep20/ir/Prog.java
@@ -1,6 +1,6 @@
 package org.teachfx.antlr4.ep20.ir;
 
-
+import org.teachfx.antlr4.ep20.ir.stmt.Label;
 import org.teachfx.antlr4.ep20.pass.cfg.BasicBlock;
 
 import java.util.ArrayList;
@@ -9,6 +9,8 @@ import java.util.List;
 
 public class Prog extends IRNode {
     public List<BasicBlock> blockList;
+
+    public List<IRNode> instrs = new ArrayList<>();
 
     public Prog() {
         this.blockList = new ArrayList<>() ;
@@ -21,5 +23,28 @@ public class Prog extends IRNode {
 
     public void addBlock(BasicBlock basicBlock) {
         blockList.add(basicBlock);
+    }
+
+    private void linearInstrsImpl(BasicBlock basicBlock) {
+        if (!basicBlock.getStmts().isEmpty()) {
+            instrs.add(new Label(basicBlock.toString(),null));
+            instrs.addAll(basicBlock.getStmts());
+        }
+
+        if (basicBlock.getSuccessors().isEmpty()) {
+            return;
+        }
+
+        for(var successor : basicBlock.getSuccessors()){
+            linearInstrsImpl(successor);
+        }
+    }
+
+    public List<IRNode> linearInstrs() {
+        for(var block : blockList) {
+            linearInstrsImpl(block);
+        }
+
+        return instrs;
     }
 }

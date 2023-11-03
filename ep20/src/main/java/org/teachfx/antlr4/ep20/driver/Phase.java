8df924f22ceeb09f22edd339d987df2804802014
willson walter <whtoo@qq.com>
2023-11-03 16:58:16 +0800

    1. add task abstraction .

---
+++ ep20/src/main/java/org/teachfx/antlr4/ep20/driver/Phase.java
@@ -0,0 +1,28 @@
+package org.teachfx.antlr4.ep20.driver;
+
+import java.util.Optional;
+
+public abstract class Phase<Input,Output> implements Task<Input,Output>,ErrorIssuer {
+    public final String name;
+
+    public Phase(String name) {
+        this.name = name;
+    }
+
+    public abstract Output transform(Input input);
+
+    public void onSucceed(Output output) {}
+
+    @Override
+    public Optional<Output> apply(Input input) {
+        var out = transform(input);
+        if (hasError()) {
+
+            printErrors(System.out);
+
+            return Optional.empty();
+        }
+
+        return Optional.of(out);
+    }
+}
---
+++ ep21/src/main/java/org/teachfx/antlr4/ep21/driver/Phase.java
@@ -0,0 +1,30 @@
+package org.teachfx.antlr4.ep21.driver;
+import org.teachfx.antlr4.ep21.error.CymbalError;
+
+import java.util.Optional;
+
+public abstract class Phase<Input,Output> implements Task<Input,Output>,ErrorIssuer {
+
+    public final String name;
+
+    public Phase(String name) {
+        this.name = name;
+    }
+
+    public abstract Output transform(Input input);
+
+    public void onSucceed(Output output) {}
+
+    @Override
+    public Optional<Output> apply(Input input) {
+        var out = transform(input);
+        if (hasError()) {
+
+            printErrors(System.out);
+
+            return Optional.empty();
+        }
+
+        return Optional.of(out);
+    }
+}

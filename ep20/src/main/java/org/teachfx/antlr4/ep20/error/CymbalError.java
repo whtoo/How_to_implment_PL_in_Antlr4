4e031c0fb0c41ee36a5112f724f2fb3c0325caf3
willson walter <whtoo@qq.com>
2023-11-03 16:57:22 +0800

    1. rename file .

---
+++ ep20/src/main/java/org/teachfx/antlr4/ep20/error/CymbalError.java
@@ -0,0 +1,37 @@
+package org.teachfx.antlr4.ep20.error;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.teachfx.antlr4.ep20.parser.Location;
+
+public abstract class CymbalError implements Comparable<CymbalError> {
+    public Location location;
+
+    public abstract String getErrMessage();
+
+    public CymbalError(Location location1) {
+        this.location = location1;
+    }
+
+    protected Location getPos() {
+        return location;
+    }
+
+    /**
+     * 返回包含位置信息在内的完整错误信息
+     */
+    @Override
+    public String toString() {
+        if (!location.hasPos()) {
+            return "*** Error: " + getErrMessage();
+        } else {
+            return "*** Error at " + location + ": " + getErrMessage();
+        }
+    }
+
+    @Override
+    @Contract(pure = true)
+    public int compareTo(@NotNull CymbalError o) {
+        return location.compareTo(o.location);
+    }
+}

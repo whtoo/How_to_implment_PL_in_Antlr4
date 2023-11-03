605c528e01a1aefc42286735920f50fe0bcd2b05
willson walter <whtoo@qq.com>
2023-11-03 16:58:05 +0800

    1. add position detection .

--- ep20/src/main/java/org/teachfx/antlr4/ep20/parser/Location.java
+++ ep20/src/main/java/org/teachfx/antlr4/ep20/parser/Location.java
@@ -1,10 +1,14 @@
 package org.teachfx.antlr4.ep20.parser;
 
-public class Location {
+public class Location implements Comparable<Location>{
     protected int startLine;
     protected int startColumn;
     protected int endLine;
     protected int endColumn;
+
+    public boolean hasPos() {
+        return startLine > 0;
+    }
 
     public Location(int startLine,int startColumn,int endLine,int endColumn){
         this.startLine = startLine;
@@ -17,5 +21,10 @@ public class Location {
     public String toString() {
         String lineRange = startLine == endLine ? " %d ".formatted(startLine) : " %d-%d ".formatted(startLine,endLine);
         return "Line"+lineRange+":"+startColumn+"-"+endColumn;
+    }
+
+    @Override
+    public int compareTo(Location o) {
+        return startLine = o.startLine;
     }
 }
--- ep21/src/main/java/org/teachfx/antlr4/ep21/parser/Location.java
+++ ep21/src/main/java/org/teachfx/antlr4/ep21/parser/Location.java
@@ -1,10 +1,17 @@
 package org.teachfx.antlr4.ep21.parser;
 
-public class Location {
+import org.jetbrains.annotations.NotNull;
+
+
+public class Location implements Comparable<Location> {
     protected int startLine;
     protected int startColumn;
     protected int endLine;
     protected int endColumn;
+
+    public boolean hasPos() {
+        return startLine >= 0;
+    }
 
     public Location(int startLine,int startColumn,int endLine,int endColumn){
         this.startLine = startLine;
@@ -17,5 +24,16 @@ public class Location {
     public String toString() {
         String lineRange = startLine == endLine ? " %d ".formatted(startLine) : " %d-%d ".formatted(startLine,endLine);
         return "Line"+lineRange+":"+startColumn+"-"+endColumn;
+    }
+
+    @Override
+    public int compareTo(@NotNull Location o) {
+        if(startLine == o.startLine) {
+            return 0;
+        } else if (startLine > o.startLine) {
+            return 1;
+        }
+
+        return -1;
     }
 }

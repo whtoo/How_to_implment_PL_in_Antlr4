7678c8444cec46551a9edfb21ad30bc094acb767
willson walter <whtoo@qq.com>
2026-05-19 17:50:05 +0800

    chore: clean up old placeholder/stub files in ep10-ep14

--- ep12/src/main/java/org/teachfx/antlr4/ep12/ast/ExpressionNode.java
+++
@@ -1,5 +0,0 @@
-package org.teachfx.antlr4.ep12.ast;
-
-public abstract class ExpressionNode {
-
-}
--- ep14/src/main/java/org/teachfx/antlr4/ep14/symtab/Type.java
+++
@@ -1,7 +0,0 @@
-package org.teachfx.antlr4.ep14.symtab;
-
-public interface Type {
-    String getName();
-
-    boolean isPrimitive();
-}
